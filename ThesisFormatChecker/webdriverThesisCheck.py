c18c51c6265f25e45a70faa894f1341f993e97b0
AutumnLai <55129510+AutumnLai@users.noreply.github.com>
2022-06-21 16:02:56 +0800

    Update webdriverThesisCheck.py

--- ThesisFormatChecker/webdriverThesisCheck.py
+++ ThesisFormatChecker/webdriverThesisCheck.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium import webdriver 
+import pandas as pd
 import os
 import re
 import shutil
@@ -52,7 +53,7 @@ def FindS(contextWithoutC):
         return i + 1
   
 
-def ContentCheck(contextWithoutC,sId,content):
+def ContentCheck(contextWithoutC,sId,content,yourContextStart):
 
     wkdir = "./"+sId + "./"
     sId = wkdir + sId
@@ -64,10 +65,81 @@ def ContentCheck(contextWithoutC,sId,content):
     uncorrectPageNum = []
 
     content #從網頁資訊抓下來的
-    needToCheckSet = content.split('\n')
-    for i in needToCheckSet:
-      print(i)
+    needToCheckSet = content.split('\n')#以換行區分
+    numberRe = re.compile(r'\d+$|[IXV]+$|[ixv]+$') # 這裡可能需要改
+    digitNumber = False
+    report = pd.DataFrame()  #報表
     
+    for checkItem in needToCheckSet:
+
+        tempCheckItem = checkItem
+        checkItem = checkItem.replace('.',"") 
+        checkItem = checkItem.replace('…',"")
+        checkItem = checkItem.replace('\n',"")
+        PageNumber = numberRe.search(checkItem)
+
+        if PageNumber != None:      
+            if str(PageNumber.group())[0] == 'I' or PageNumber.group()[0] == 'X' or PageNumber.group()[0] == 'V' or str(PageNumber.group())[0] == 'i' or str(PageNumber.group())[0] == 'x' or str(PageNumber.group())[0] == 'v':
+                pageN=romanToInt(PageNumber.group() )
+                if digitNumber :
+                    break
+            else :
+                digitNumber = True
+                try:
+                    pageN = int(PageNumber.group()) -1 +yourContextStart-1
+                except:
+                    pageN=0
+                    
+            print(str(checkItem) + "number is "+str(pageN))
+          
+            titleRe = re.compile(r'\D*')
+            titleContextG = titleRe.search(checkItem )
+            if titleContextG != None:
+                titleContext = titleContextG.group()
+                titleContext = titleContext.replace('.',"")
+                titleContext = titleContext.replace('…',"")
+                titleContext = titleContext.replace('\n',"")
+            
+            #print(titleContext)
+                try :
+                    titleContext=titleContext.replace('\t',"")
+                    titleContext=titleContext.replace(' ',"")
+                    titleCheckRe = re.compile(titleContext)
+                    ContextDeleteEnter = contextWithoutC[pageN].replace('\n',"")
+                    ContextDeleteEnter = ContextDeleteEnter.replace('\t',"")
+                    ContextDeleteEnter = ContextDeleteEnter.replace(' ',"")
+                    ContextDeleteEnter = ContextDeleteEnter.replace('.',"")
+                    ContextDeleteEnter = ContextDeleteEnter.replace('...',"")
+                    titleCheckG = titleCheckRe.search(ContextDeleteEnter)
+                    itemNum = itemNum + 1
+                    if titleCheckG != None:
+                        correctNum = correctNum + 1
+                    else :
+                        uncorrect = uncorrect +1 
+                        uncorrectList.append(titleContext)
+                        uncorrectPageNum.append(pageN-yourContextStart+2)
+                        print(titleContext)
+                        checkBool = False 
+                except:
+                    uncorrect = uncorrect +1 
+                    uncorrectList.append("沒標題")
+                    uncorrectPageNum.append(pageN-yourContextStart+2)
+                        
+                    checkBool = False 
+            
+            else :
+                print("something error")
+                    
+        #找出索引頁碼，對應contextWithoutC的index
+    
+    #print("符合率"+str(correctNum/itemNum))
+    print("檢驗的項目數"+str(itemNum))
+    print("相符的項目數"+str(correctNum))
+    print("錯誤的項目數"+str(uncorrect))
+    report = pd.DataFrame((zip(uncorrectList, uncorrectPageNum)), columns = ['Ttitle', 'pageNum'])
+
+    report.to_csv(sId+"report.csv",encoding = "utf_8_sig")
+    return checkBool
     
 #下載位置設定
 
@@ -81,8 +153,8 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://cloud.ncl.edu.tw/cycu/in.php?school_id=13")
 
 sleep(1)
-driver.find_element_by_name("FO_userid").send_keys("userId")
-driver.find_element_by_name("FO_passwd").send_keys("Password")
+driver.find_element_by_name("FO_userid").send_keys("user_id")
+driver.find_element_by_name("FO_passwd").send_keys("password")
 driver.find_element_by_name("Image6").click()
 #登入
 
@@ -113,7 +185,7 @@ for i in range(len(all_thesis)):
   driver.find_element_by_id("ui_6").click()
   driver.find_element_by_partial_link_text("Full-Text").click()#下載好了
   #driver.find_element_by_partial_link_text("查看").click()#下載好了、授權書檔名不一樣，沒辦法直接丟 ，應該有辦法記住它的名稱
-  sleep(10)
+  sleep(5)
 #把檔案移過去
   source = os.getcwd()+ "/"+"/Full-Text.pdf"
   destination = os.getcwd()+ "/"+str(sId) +"/"+"/Full-Text.pdf"
@@ -132,14 +204,13 @@ for i in range(len(all_thesis)):
   contextWithoutC = readContext(pdfReader)
   sPage = FindS(contextWithoutC)
   print(sPage)
-  ContentCheck(contextWithoutC,sId,content)
+  ContentCheck(contextWithoutC,sId,content,sPage)
   sleep(3)
 
   driver.close()
   driver.switch_to.window(home_page)
 
-# 接下來想辦法找到ContentCheck所需要的值
-#(contextWithoutC,yourContentPageS,yourContentPageE,yourContextStart,fileName):
+
 
 
 
